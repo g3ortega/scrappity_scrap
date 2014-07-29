8148a2c9c090aaa1f6471a5fcc6104b04cb7c416
Gerardo Ortega <geraldavid7@gmail.com>
2014-07-29 07:38:12 -0600

    Otros fixes correlación

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,3 @@
 module ApplicationHelper
-  def mobile_device?
-    request.user_agent =~ /Mobile|webOS/
-  end
-
-  def submit_text
-    mobile_device? ? "Search" : "Search RailsConf Abstracts!"
-  end
 
 end
